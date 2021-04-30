469da85a3cf14f018e26b2ffabda53e4131dce8d
erickmantai <erickmantai@gmail.com>
2021-04-29 23:12:07 -0300

    finAPI ignite

---
+++ src/index.js
@@ -0,0 +1,104 @@
+const { request } = require("express");
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
+
+const app = express();
+
+app.use(express.json());
+
+const customers = [];
+
+// Middlewere
+function verifyIfExistsAccountCPF(request, response, next) {
+    const { cpf } = request.headers;
+
+    const customer = customers.find(customer => customer.cpf === cpf);
+
+    if (!customer) {
+        return response.status(400).json({ error: "Customer not found!" })
+    }
+
+    request.customer = customer; //exporta customer para todas as rotas que irao consumir o middleware. 
+
+    return next();
+}
+//end Middlewere
+
+function getBalance(statement) {
+    const balance = statement.reduce((acc, operation) => {
+        if (operation.type === 'credit') {
+            return acc + operation.amount;
+        } else {
+            return acc - operation.amount;
+        }
+    }, 0);
+
+    return balance;
+}
+
+app.post("/account", (request, response) => {
+    const { cpf, name } = request.body;
+
+    const customersAllreadyExists = customers.some(
+        (customer) => customer.cpf === cpf
+    );
+
+    if (customersAllreadyExists)
+        return response.status(400).json({ error: "Customer already exists!" });
+
+    customers.push({
+        cpf,
+        name,
+        id: uuidv4(),
+        statement: []
+    });
+
+    return response.status(201).send();
+});
+
+app.get("/statement/", verifyIfExistsAccountCPF, (request, response) => {
+
+    const { customer } = request; //captura customer de dentro do middleware
+
+    return response.json(customer.statement);
+});
+
+app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
+    const { description, amount } = request.body;
+
+    const { customer } = request; //captura customer do middleware
+
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: "credit"
+    }
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
+});
+
+app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
+    const { amount } = request.body;
+    const { customer } = request; //captura customer do middleware
+
+    const balance = getBalance(customer.statement);
+
+    if (balance < amount) {
+        return response.status(400).json({ error: "Insufficient funds" });
+    }
+
+    const statementOperation = {
+        amount,
+        created_at: new Date(),
+        type: "debit"
+    }
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
+});
+
+app.listen(3333);
